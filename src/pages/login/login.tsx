a25c6f8c1f5ef85db79ad01256770f6c035a447f
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-09-10 02:02:52 +0700

    test antd login form

---
+++ src/pages/login/login.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import LoginForm from "../../modules/login-form/login-form";
+
+export default function Login(): JSX.Element {
+  return <LoginForm />;
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { useRoutes } from "react-router-dom";
 import HomeLayout from "../layouts/home-layout";
 import Home1 from "../pages/home1/home1";
 import Home2 from "../pages/home2/home2";
+import Login from "../pages/login/login";
 
 export default function Router() {
   const routing = useRoutes([
@@ -21,6 +22,10 @@ export default function Router() {
         {
           path: "/home2",
           element: <Home2 />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
       ],
     },
